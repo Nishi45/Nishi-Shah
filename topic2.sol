de219ef8c152d2852be9079310d376ec5da3a2b3
Nishi Shah <nishipshah1999@gmail.com>
2023-02-28 12:58:27 +0530

    initial commit

---
+++ topic2.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract counter{
+    uint public Count;
+
+    function getcount() public view returns(uint){
+        return Count;
+    }
+    function count1more() public{
+        Count++;
+    }
+
+
+}
